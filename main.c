53ecf9fafadd535d78a5c79d82bf1f606c283ae6
jrasb <okrypczyk@protonmail.com>
2023-03-13 21:42:48 +0100

    yuno jumpscare

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
-#include <stdio.h>
+#include <raylib.h>
+#include <stdlib.h>
 
 int main(void) {
-    printf("hello world");
+    int screenHeight = 720;
+    int screenWidth = 1280;
+
+    Texture2D backgroundImage;
+
+    InitWindow(screenWidth, screenHeight, "Raylib!");
+
+    backgroundImage = LoadTexture("./resources/textures/yuno.png");
+    
+    SetTargetFPS(144);
+
+    while(!WindowShouldClose()) {
+        /*KEY PRESS EVENTS HERE*/
+        if (IsKeyPressed(KEY_F11)) {
+            ToggleFullscreen();
+        }
+
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(backgroundImage, 100, 100, WHITE);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+    CloseAudioDevice();
 
     return 0;
 }
