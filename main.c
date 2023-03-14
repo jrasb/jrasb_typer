73a39d2742cce68c4f0b182c4dbb2e948c777cf9
jrasb <okrypczyk@protonmail.com>
2023-03-14 11:49:19 +0100

    yuno jumpscare 2.0

--- main.c
+++ main.c
@@ -9,9 +9,13 @@ int main(void) {
     const int screenHeight = 720;
     const int screenWidth = 1280;
 
+    Texture2D yuno;
+
     /* Initialise window and audio */
     InitWindow(screenWidth, screenHeight, "Jrasb Typer");
     InitAudioDevice();
+
+    yuno = LoadTexture("./resources/textures/yuno.png");
 
     SetTargetFPS(144);
 
@@ -27,6 +31,8 @@ int main(void) {
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
+
+        DrawTexture(yuno, 0, 0, WHITE);
         
         EndDrawing();
     }
