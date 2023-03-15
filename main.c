9a7af1f56615fa878d2d20e5a0bdb69ca39522dd
jrasb <okrypczyk@protonmail.com>
2023-03-15 11:39:44 +0100

    figured out typing functionality

--- main.c
+++ main.c
@@ -1,19 +1,21 @@
 #include <raylib.h>
 #include <stdlib.h>
 
-char paragraphs[] = {
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-};
+#define MAX_CHARS 128
 
 int main(void) {
     const int screenHeight = 720;
     const int screenWidth = 1280;
 
     Texture2D yuno;
+    Rectangle textBox = { screenWidth/2.0f - 100, 180, 255, 0} ;
 
     /* Initialise window and audio */
     InitWindow(screenWidth, screenHeight, "Jrasb Typer");
     InitAudioDevice();
+
+    char text[MAX_CHARS] = "\0";
+    int letterCount = 0;
 
     yuno = LoadTexture("./resources/textures/yuno.png");
 
@@ -22,18 +24,38 @@ int main(void) {
     /* Main game loop */
     while(!WindowShouldClose()) {
 
+        int key = GetCharPressed();
+
+        while (key > 0) {
+            if ((key >= 32) && (key <= 125) && (letterCount < MAX_CHARS)) {
+                text[letterCount] = (char)key;
+                text[letterCount+1] = '\0';
+                letterCount++;
+            }
+
+            key = GetCharPressed();
+        }
+
+        if (IsKeyPressed(KEY_BACKSPACE)) {
+            letterCount--;
+            if (letterCount < 0) letterCount = 0;
+            text[letterCount] = '\0';
+        }
+
         /* KEY PRESS EVENTS HERE */
         if (IsKeyPressed(KEY_F11)) {
             ToggleFullscreen();
         }
+
 
         /* Drawing begins here*/
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawTexture(yuno, 0, 0, WHITE);
-        
+        DrawRectangleRec(textBox, LIGHTGRAY);
+        DrawText(text, (int)textBox.x + 5, (int)textBox.y + 8, 40, MAROON);
+
         EndDrawing();
     }
 
